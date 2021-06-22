58558db2854e4abdcbe5b5228c52af62d1adb669
Luis Reyes <luisreyesxv@yahoo.com>
2021-06-22 12:54:09 -0400

    addressed the second issue with ticket 2. changed the order of the messages in the conversations so that they are in ascending order.

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -20,7 +20,7 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "DESC"]],
+      order: [[Message, "createdAt", "ASC"]],
       include: [
         { model: Message, order: ["createdAt", "DESC"] },
         {
@@ -69,7 +69,7 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length - 1 ].text;
       conversations[i] = convoJSON;
     }
 
